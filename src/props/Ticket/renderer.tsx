f10b59376c86c838124a9b1d002677be794d5ae5
Tom Gray <tom.c.gray@gmail.com>
2021-05-27 23:45:04 +0100

    some padding stuff

--- src/props/Ticket/renderer.tsx
+++ src/props/Ticket/renderer.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import { PAPER_TYPES } from "../../config";
 
 export const Ticket = (props: typeof PAPER_TYPES["TICKET"]["data"]) => {
+  console.log("props.sawtooth_border", props.sawtooth_border);
   return (
     <div className="relative mx-auto flex justify-around">
       <div
@@ -13,58 +14,72 @@ export const Ticket = (props: typeof PAPER_TYPES["TICKET"]["data"]) => {
         }}
         className={`paper absolute transition paper-${props.paper_texture} overflow-hidden transition transform mx-auto ${props.rounded_corners}`}
       >
-        <div className="w-full h-full p-8">
+        <div
+          className={classNames("w-full h-full", {
+            "p-8": props.sawtooth_border === "sawtooth-border-none",
+            "px-8 py-10": props.sawtooth_border === "sawtooth-border-y",
+            "px-10 py-8": props.sawtooth_border === "sawtooth-border-x",
+          })}
+        >
           <table className="table-fixed border-collapse w-full h-full">
-            <tr>
-              <td
-                rowSpan={3}
-                className={classNames(
-                  "border border-gray-900 border-double w-14",
-                  {
-                    hidden: props.hide_left_margin_copy,
-                  }
-                )}
-              >
-                <span
-                  style={{
-                    writingMode: "vertical-lr",
-                  }}
-                  className="block transform rotate-180 ml-3 font-bold"
+            <tbody>
+              <tr>
+                <td
+                  rowSpan={3}
+                  className={classNames(
+                    "border border-gray-900 border-double w-14",
+                    {
+                      hidden: props.hide_left_margin_copy,
+                    }
+                  )}
                 >
-                  {props.left_margin_copy}
-                </span>
-              </td>
-              <td colSpan={2} className="border border-gray-900 border-double">
-                XX 1
-              </td>
-              <td
-                rowSpan={3}
-                className={classNames(
-                  "border border-gray-900 border-double w-14",
-                  {
-                    hidden: props.hide_right_margin_copy,
-                  }
-                )}
-              >
-                <span
-                  style={{
-                    writingMode: "vertical-lr",
-                  }}
-                  className="block transform rotate-180 ml-3 font-bold"
+                  <span
+                    style={{
+                      writingMode: "vertical-lr",
+                    }}
+                    className="block transform rotate-180 ml-3 font-bold"
+                  >
+                    {props.left_margin_copy}
+                  </span>
+                </td>
+                <td
+                  colSpan={2}
+                  className="border border-gray-900 border-double"
                 >
-                  {props.right_margin_copy}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td className="border border-gray-900 border-double">XX 2</td>
-              <td className="border border-gray-900 border-double">YY 2</td>
-            </tr>
-            <tr>
-              <td colSpan={2} className="border border-gray-900 border-double">
-                XX 3
-              </td>
-            </tr>
+                  XX 1
+                </td>
+                <td
+                  rowSpan={3}
+                  className={classNames(
+                    "border border-gray-900 border-double w-14",
+                    {
+                      hidden: props.hide_right_margin_copy,
+                    }
+                  )}
+                >
+                  <span
+                    style={{
+                      writingMode: "vertical-lr",
+                    }}
+                    className="block transform rotate-180 ml-3 font-bold"
+                  >
+                    {props.right_margin_copy}
+                  </span>
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-gray-900 border-double">XX 2</td>
+                <td className="border border-gray-900 border-double">YY 2</td>
+              </tr>
+              <tr>
+                <td
+                  colSpan={2}
+                  className="border border-gray-900 border-double"
+                >
+                  XX 3
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
