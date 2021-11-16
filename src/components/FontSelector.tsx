749286c7d3810c8b50fea13d62e9dbc9dee976f7
Tom Gray <tom.c.gray@gmail.com>
2021-11-16 10:18:54 +0000

    more fonts

--- src/components/FontSelector.tsx
+++ src/components/FontSelector.tsx
@@ -18,26 +18,65 @@ export const FontSelector = (props: iFontSelector) => {
             props.onUpdate(e.target.value);
           }}
         >
+          {/* serif fonts */}
           <option className="font-serif" value="font-serif">
             Standard Web Serif
           </option>
+          <option className="font-alfa-slab-one" value="font-alfa-slab-one">
+            ↪ Alfa Slab One
+          </option>
+          <option className="font-newsreader" value="font-newsreader">
+            ↪ Newsreader
+          </option>
+          <option className="font-noticia-text" value="font-noticia-text">
+            ↪ Noticia Text
+          </option>
+          <option className="font-yuji-syuku" value="font-yuji-syuku">
+            ↪ Yuji Syuku
+          </option>
+          <option className="font-crete-round" value="font-crete-round">
+            ↪ Crete Round
+          </option>
+          <option
+            className="font-playfair-display-sc"
+            value="font-playfair-display-sc"
+          >
+            ↪ Playfair Display SC
+          </option>
+          <option className="font-quattrocento" value="font-quattrocento">
+            ↪ Quattrocento
+          </option>
+
+          <option className="font-im-fell" value="font-im-fell">
+            ↪ IM Fell
+          </option>
+
+          <option className="font-im-fell-display" value="font-im-fell-display">
+            ↪ IM Fell Display
+          </option>
+
+          {/* sans serif fonts */}
+
           <option className="font-sans" value="font-sans">
             Standard Web Sans
           </option>
-          <option className="font-alfa-slab-one" value="font-alfa-slab-one">
-            Alfa Slab One
-          </option>
-          <option className="font-newsreader" value="font-newsreader">
-            Newsreader
-          </option>
-          <option className="font-noticia-text" value="font-noticia-text">
-            Noticia Text
+
+          <option className="font-cursive" value="font-cursive">
+            Standard Web Cursive
           </option>
           <option className="font-indie-flower" value="font-indie-flower">
-            Indie Flower
+            ↪ Indie Flower
           </option>
           <option className="font-dancing-script" value="font-dancing-script">
-            Dancing Script
+            ↪ Dancing Script
+          </option>
+
+          <option className="font-fantasy" value="font-fantasy">
+            Standard Web Fantasy
+          </option>
+
+          <option className="font-medieval-sharp" value="font-medieval-sharp">
+            ↪ Medieval Sharp
           </option>
         </select>
       </div>
