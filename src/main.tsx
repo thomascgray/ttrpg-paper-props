7f91cc3029a8c66a70e01e511e0c5c2b47c9451a
Tom Gray <tom.c.gray@gmail.com>
2025-06-29 22:22:40 +0100

    fix-dev-build-processes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import "./build.css";
+import "./index.css";
 import "./ink.css";
 import App from "./App";
 
